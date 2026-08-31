e6db9ddebc652491bd5d06d2a9e712b7158d9bfd
ljingqi <ljingqi@users.noreply.github.com>
2026-08-31 23:20:13 +0800

    Remove useless folder

--- _scratch/verify_final.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-idx = open(r"D:/Journal/_scratch/sess_map_test/index.html", encoding="utf-8").read()
-print('class="map-figure" present:', 'class="map-figure"' in idx)
-print("svg count:", idx.count("<svg"))
-print("map-figure count:", idx.count("map-figure"))
-print("leftover data-chart=map:", idx.count('data-chart="map"'))
